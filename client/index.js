dd0d5b8fae6102b0d6b641d038e5eb0e82853bcd
sjquant <seonujang92@gmail.com>
2020-10-05 15:43:36 +0900

    Create simple calculator ui

---
+++ client/index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
+
+function createWindow() {
+  const win = new BrowserWindow({
+    minWidth: 800,
+    minHeight: 600,
+    maxWidth: 800,
+    maxHeight: 600,
+    title: "Simple Calculator",
+    useContentSize: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  win.setMenuBarVisibility(false);
+  win.loadFile("index.html");
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});

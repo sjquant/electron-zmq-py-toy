ec3bb93f28159b4c705755128bdded4c84175d93
sjquant <seonujang92@gmail.com>
2020-10-07 16:07:16 +0900

    Create python part

---
+++ server/core.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
---
+++ server/main.py
@@ -0,0 +1,45 @@
+import zmq
+import time
+import json
+
+from core import Calculator
+
+addr = 'tcp://127.0.0.1:5555'
+ctx = zmq.Context()
+socket = ctx.socket(zmq.REP)
+socket.bind(addr)
+calculator = Calculator()
+
+class NoCommandError(Exception):
+    pass
+
+class ProgramExit(Exception):
+    pass
+
+def process_message(message):
+    data = json.loads(message)
+    command = data["command"]
+    args = data.get("args", [])
+    if hasattr(calculator, command):
+        func = getattr(calculator, command)
+        ret = func(*args)
+        return {"result": ret}
+    elif command == "Exit":
+        raise ProgramExit
+    else:
+        raise NoCommandError(f"No such command : '{command}'")
+
+def main():
+    while True:
+        message = socket.recv_string()
+        try:
+            ret = process_message(message)
+            ret = json.dumps(ret)
+            socket.send(ret.encode()) 
+        except NoCommandError:
+            continue
+        except ProgramExit:
+            break
+
+if __name__ == "__main__":
+    main()
